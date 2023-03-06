a8d6f1a6d2f999777434f0e8f36aafdf8a400db2
Alcatraz2033 <alcatrazthelman@gmail.com>
2023-03-06 14:20:47 -0500

    Actualizancion

--- Thelman_Pabon_examen2.cpp
+++ Thelman_Pabon_examen2.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "lib/TPcolors.h"
 #include "lib/TPp_bar.h"
 using namespace std;
 
-int main(){
-	cout<<"Iniciando el examen\n";
+const string TPnomre = "Thelman Jihat Pabon Quevedo";
+const int TPcedula = 1500826555;
 
+void TPlectura();
+void TPdatos();
+
+int main(){
+	//TP_loading();
+	cout<<"\n["<<MAGENTA<<"+"<<MAGENTA<<"] Leyendo los datos...\n\n"<<RESET;
+	TPlectura();
+	TPdatos();
 	return 0;
+}
+
+void TPlectura(){
+	ifstream archivo; 
+	string TPtexto;
+	system("cat coordenadas.txt | uniq > sinrep.txt");
+	archivo.open("sinrep.txt",ios::in); //Abrimos el archivoo en lectura
+	if(archivo.fail()){
+		cout<<RED"[!] NO SE PUDO ABRIR EL ARCHIVO\n\n"<<RESET;
+		exit(1);
+	}
+	while(!archivo.eof()){ //mientras no sea el fin al del archivo;
+		getline(archivo,TPtexto);
+		if(TPtexto.find("Cap") != string::npos){
+			TP_loading();
+			cout<<RED<<" [!] "<<TPtexto<<endl;
+		}else{
+			TP_loading();
+			cout<<LGREEN<<" [+] "<<TPtexto<<endl;
+		}
+	}system("rm sinrep.txt");
+}
+
+void TPdatos(){
+	cout<<WHITE<<"\n\n["<<GREEN<<"+"<<WHITE<<"] "<<GREEN<<"Informacion del Arbol binario de capacidad belica Ucraniana\n\n"<<RESET;
+	cout<<WHITE<<"["<<GREEN<<"~"<<WHITE<<"] "<<GREEN<<"Developer-Nombre: "<<RESET<<TPnomre<<endl;
+	cout<<"["<<GREEN<<"~"<<WHITE<<"] "<<GREEN<<"Developer-Cedula: "<<RESET<<TPcedula<<endl;
 }
--- lib/TPp_bar.h
+++ lib/TPp_bar.h
@@ -4,7 +4,7 @@
 #include "TPcolors.h"
 using namespace std;
 
-void p_bar(string icon, float lengt, float time){ // Icono █, Largo, Tiempo 
+void TP_bar(string icon, float lengt, float time){ // Icono █, Largo, Tiempo 
 	string x = icon;float porc = 1; float timer = time * 1000000;
 	string v = "";
 	for(int i=0; i<=lengt; i++){
@@ -15,6 +15,23 @@ void p_bar(string icon, float lengt, float time){ // Icono █, Largo, Tiempo
 			v = "/";
 		}
 		porc = (100/lengt) * i;cout<<WHITE<<"["<<CYAN<<v<<WHITE<<"] Cargando "<<CYAN<<lround(porc)<<"%"<<WHITE<<": "<<GREEN<<x<<endl;x += icon;
+		usleep(timer);
+	}
+}
+
+void TP_loading(){ // Icono █, Largo, Tiempo 
+	string icon = ".";
+	float porc = 1; 
+	float timer = 0.005 * 1000000;
+	string v = "";
+	for(int i=0; i<=100; i++){
+		if(i%2 == 0){
+			v = "->";
+		}else{
+			v = "<-";
+		}
+		porc = (1/1) * i;
+		cout<<WHITE<<"\r["<<CYAN<<v<<WHITE<<"] Cargando "<<CYAN<<lround(porc)<<"%"<<WHITE<<""<<GREEN;
 		usleep(timer);
 	}
 }
