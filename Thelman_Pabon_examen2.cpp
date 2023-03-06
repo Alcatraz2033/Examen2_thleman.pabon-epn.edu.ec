1b32b0dc87f0b54bbed9b959bbee3e68b0190804
Alcatraz2033 <alcatrazthelman@gmail.com>
2023-03-06 15:45:16 -0500

    Arbol añadido

--- Thelman_Pabon_examen2.cpp
+++ Thelman_Pabon_examen2.cpp
@@ -1,22 +1,118 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include "lib/TPcolors.h"
 #include "lib/TPp_bar.h"
 using namespace std;
 
-const string TPnomre = "Thelman Jihat Pabon Quevedo";
-const int TPcedula = 1500826555;
+struct TPcoordenada{
+	int TPcapacidad_belica;
+	int TPgeolocalizacion;
+	char TParsenal[20];
+	int *der;
+	int *izq;
+};
 
+struct Nodo{
+	int TPdato;
+	Nodo *TPder;
+	Nodo *TPizq;
+};
+
+Nodo *TPcrear_nodo(int);
+Nodo *TParbol = NULL;
+
+void TPinsertar_nodo(Nodo *&,int);
 void TPlectura();
 void TPdatos();
+void TParchivo_al_arbol();
+string TP_commd(string);
+void fflushin();
+
+const string THELMAN_PABON = "Thelman Jihat Pabon Quevedo";
+const int TPCEDULA = 1500826555;
 
 int main(){
-	//TP_loading();
 	cout<<"\n["<<MAGENTA<<"+"<<MAGENTA<<"] Leyendo los datos...\n\n"<<RESET;
 	TPlectura();
 	TPdatos();
+	TParchivo_al_arbol();
 	return 0;
+}
+
+void TParchivo_al_arbol(){
+	string TPcomando = TP_commd("cat coordenadas.txt | awk '{print $1}' | tr -d ',' | tr -d 'Cap'  | uniq | sed '/^\\s*$/d'");
+	for(char c : TPcomando){
+		TPinsertar_nodo(TParbol, c);
+	}
+}
+
+void fflushin(){ // funcion para calular la basura del buffer y eliminarla
+
+ int d, i=0;
+ 	d = stdin->_IO_read_end - stdin->_IO_read_ptr;
+	for(i=0; i<d; i++)
+	getchar();
+}
+
+string TP_commd(string commando){
+    // Comando a ejecutar
+    //string command = "networkctl 2>/dev/null |  awk '{print $(NF-3)}' 2>/dev/null | grep -v 'LINK'";
+    string command = commando;
+
+    // Nombre del archivo temporal
+    string filename = "temp.txt";
+
+    // Ejecutar el comando y redirigir la salida al archivo temporal
+    stringstream ss;
+    ss << command << " > " << filename;
+    int result = system(ss.str().c_str());
+
+    if (result == 0) {
+        // Abrir el archivo temporal y leer su contenido en una variable
+        ifstream file(filename);
+        string output;
+        if (file.is_open()) {
+            stringstream ss_output;
+            ss_output << file.rdbuf();
+            output = ss_output.str();
+            file.close();
+        }
+
+        // Eliminar el archivo temporal
+        remove(filename.c_str());
+
+        // Devolver la salida del comando
+        return output;
+    } else {
+        cerr << "Error al ejecutar el comando\n";
+        return "";
+        // o también puedes lanzar una excepción para indicar el error
+        // throw runtime_error("Error al ejecutar el comando");
+    }
+}
+
+void TPinsertar_nodo(Nodo *&TParbol, int n){
+	if(TParbol == NULL){
+		Nodo *TPnuevo_nodo = TPcrear_nodo(n);
+		TParbol = TPnuevo_nodo;
+	}else{
+		int TPvalor_raiz = TParbol->TPdato;
+		if(n < TPvalor_raiz){
+			TPinsertar_nodo(TParbol->TPizq,n);
+		}else{
+			TPinsertar_nodo(TParbol->TPder,n);
+		}
+	}
+}
+
+Nodo *TPcrear_nodo(int n){
+	Nodo *TPnuevo_nodo = new Nodo();
+	TPnuevo_nodo->TPdato = n;
+	TPnuevo_nodo->TPder = NULL;
+	TPnuevo_nodo->TPizq = NULL;
+	return TPnuevo_nodo;
 }
 
 void TPlectura(){
@@ -31,8 +127,7 @@ void TPlectura(){
 	while(!archivo.eof()){ //mientras no sea el fin al del archivo;
 		getline(archivo,TPtexto);
 		if(TPtexto.find("Cap") != string::npos){
-			TP_loading();
-			cout<<RED<<" [!] "<<TPtexto<<endl;
+			cout<<RED<<"[!]ERROR\t   [!] "<<TPtexto<<endl;
 		}else{
 			TP_loading();
 			cout<<LGREEN<<" [+] "<<TPtexto<<endl;
@@ -42,6 +137,6 @@ void TPlectura(){
 
 void TPdatos(){
 	cout<<WHITE<<"\n\n["<<GREEN<<"+"<<WHITE<<"] "<<GREEN<<"Informacion del Arbol binario de capacidad belica Ucraniana\n\n"<<RESET;
-	cout<<WHITE<<"["<<GREEN<<"~"<<WHITE<<"] "<<GREEN<<"Developer-Nombre: "<<RESET<<TPnomre<<endl;
-	cout<<"["<<GREEN<<"~"<<WHITE<<"] "<<GREEN<<"Developer-Cedula: "<<RESET<<TPcedula<<endl;
+	cout<<WHITE<<"["<<GREEN<<"~"<<WHITE<<"] "<<GREEN<<"Developer-Nombre: "<<RESET<<THELMAN_PABON<<endl;
+	cout<<"["<<GREEN<<"~"<<WHITE<<"] "<<GREEN<<"Developer-Cedula: "<<RESET<<TPCEDULA<<endl;
 }
